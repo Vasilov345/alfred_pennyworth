c89d8b3a9d42cabdea0fa957dfcb51455446bda7
dvasilov <vasilov345@gmail.com>
2020-10-02 20:17:07 +0300

    Add python homework

---
+++ LinuxIntro/homework.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def calculate_ships(data: List[List[int]]):
+    """
+    Calculate ships number on field for sea battle
+
+    Args:
+        data: Two dimension array
+
+    Returns: number of ships in int value
+    Examples:
+        calculate_ships(
+        [1, 0, 0],
+        [0, 0, 1])
+        >>> 2
+    """
+    pass
