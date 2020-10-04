2e94cc9dec5e261eab644357e5b6f5bbafde326c
bezuhlyiv <karamboll509@gmail.com>
2020-10-04 20:37:10 +0300

    GitIntro/homework.py

--- GitIntro/homework.py
+++ GitIntro/homework.py
@@ -1,23 +1,15 @@
 from typing import List
+from itertools import chain
 
 
 def calculate_rooms_number(data: List[List[int]]) -> int:
-    """
-    There is a list of meetings for office.
-    Each meeting should have free room.
-    Calculate how many rooms we need to prevent conflicts.
-
-    Args:
-        data: List of meetings with time of start and time of end
-
-    Returns: Number of rooms
-
-    Examples:
-        calculate_rooms_number([[1, 2], [2, 4]])
-        >>> 2
-        calculate_rooms_number([[1, 2], [3, 4]])
-        >>> 1
-        calculate_rooms_number([[1, 2], [3, 4], [1, 5], [6, 7]])
-        >>> 2
-    """
-    pass
+    room_number = 0
+    if not data:
+        return room_number
+    else:
+        data = list(chain.from_iterable(data))
+        data1 = len(data)
+        data = set(data)
+        data2 = len(data)
+        room_number = data1 - data2 + 1
+        return room_number
