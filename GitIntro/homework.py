66b58549c84a33ac812e4b60f079392eba9f65c0
Dmytro Vons <dmytrovons@gmail.com>
2020-10-05 23:50:27 +0300

    delete mistakes

--- GitIntro/homework.py
+++ GitIntro/homework.py
@@ -2,14 +2,18 @@ from typing import List
 
 
 def calculate_rooms_number(data: List[List[int]]) -> int:
-    minTime = data[0][0]
-    maxTime = data[-1][1]
-    maxRoomsNumber = 0
-    for time in range(minTime, maxTime):
-        roomNumber = 0
+    if not data:
+        maxRoomsNumber = 0
+    else:
+        minTime = data[0][0]
+        maxTime = data[-1][1]
+        maxRoomsNumber = 0
+        for time in range(minTime, maxTime):
+            roomNumber = 0
         for meeting in data:
             if (meeting[0] <= time and time <= meeting[1]):
                 roomNumber += 1
-        if roomNumber > maxRoomsNumber:
-            maxRoomsNumber = roomNumber
+            if roomNumber > maxRoomsNumber:
+                maxRoomsNumber = roomNumber
+
     return maxRoomsNumber
