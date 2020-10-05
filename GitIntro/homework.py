89472bef66c6517418fcadaf90ba52e350f50248
Alex Zasanskyi <o.zasanskyi@ukr.net>
2020-10-05 17:25:44 +0300

    fix

--- GitIntro/homework.py
+++ GitIntro/homework.py
@@ -6,12 +6,9 @@ def calculate_rooms_number(data: List[List[int]]) -> int:
     There is a list of meetings for office.
     Each meeting should have free room.
     Calculate how many rooms we need to prevent conflicts.
-
     Args:
         data: List of meetings with time of start and time of end
-
     Returns: Number of rooms
-
     Examples:
         calculate_rooms_number([[1, 2], [2, 4]])
         >>> 2
@@ -23,13 +20,7 @@ def calculate_rooms_number(data: List[List[int]]) -> int:
     rooms_needed = 1
     if not data:
         return rooms_needed - 1
-    for i in range(len(data)):
-        if i < len(data) - 1:
-            if data[i][1] >= data[i+1][0]:
-                rooms_needed += 1
-    print(rooms_needed)
+    for i in range(len(data) - 1):
+        if data[i][1] >= data[i + 1][0]:
+            rooms_needed += 1
     return rooms_needed
-
-
-
-
