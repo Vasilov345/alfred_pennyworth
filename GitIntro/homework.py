22752cf5eae69d8447d3b89f48fbc98f3a7b354d
Valentyn-Lukashuk <Valentynlukashuk@gmail.com>
2020-10-07 21:54:09 +0300

    fi

--- GitIntro/homework.py
+++ GitIntro/homework.py
@@ -2,8 +2,7 @@ from typing import List
 
 
 def calculate_rooms_number(data: List[List[int]]) -> int:
-
-    rooms =0
+    rooms = 0
     for m in range(len(data)):
         for n in range(len(data[m])):
             if data[0][n] >= data[m - 1][n - 1]:
