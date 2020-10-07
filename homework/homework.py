a49ff60d28f35b211a2f95f08589315f1347180f
Valentyn-Lukashuk <Valentynlukashuk@gmail.com>
2020-10-07 22:46:20 +0300

    fix

---
+++ homework/homework.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def calculate_rooms_number(data: List[List[int]]) -> int:
+    
+    rooms = 0
+    for m in range(len(data)):
+        for n in range(len(data[m])):
+            if data[0][n] >= data[m-1][n-1]:
+    return rooms
+    
