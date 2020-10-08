1fa45246ce661e0388d96798632ef872e1678ceb
VolodymyrKM <kolomiecnew@gmail.com>
2020-10-08 11:50:29 +0300

    correction done homework advanced

--- AdvancedDataTypes/homework/homework_advanced.py
+++ AdvancedDataTypes/homework/homework_advanced.py
@@ -1,9 +1,5 @@
 import string
 import random
-import collections
-from typing import List, Dict
-
-Alphabet = List[Dict[str, int]]
 
 def generate_alphabet():
     keys_list = []
@@ -19,20 +15,19 @@ def generate_alphabet():
     keys_alphabet_list = dict(zip(alphabet_list, run_list))
     for x in alphabet_list:
         keys_list.append({x:keys_alphabet_list[x]})
-    print(keys_list)
     return keys_list
 
-
-def task_2_remove_dict_fields(l2):
+def sort_alphabet(l2):
     l3 = []
     s = {}
-    s_1 = []
+    sorted_alphabet = []
     for k in l2:
         l3.append(k)
         for j, d in k.items():
             s[j] = d
     s = {k: v for k, v in sorted(s.items(), key=lambda item: item[1])}
     for x in s:
-        s_1.append({x: s[x]})
-    print(s_1)
-    return s_1
+        sorted_alphabet.append({x: s[x]})
+    print(sorted_alphabet)
+    return sorted_alphabet
+
