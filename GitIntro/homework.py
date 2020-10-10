4cc3db58ad810c3e33c6da5fb408f8926795acd8
bezuhlyiv <karamboll509@gmail.com>
2020-10-10 15:33:58 +0300

    AdvancedTypes

--- GitIntro/homework.py
+++ GitIntro/homework.py
@@ -1,15 +1,12 @@
 from typing import List
-from itertools import chain
 
 
 def calculate_rooms_number(data: List[List[int]]) -> int:
-    room_number = 0
+    count_room = 1
     if not data:
-        return room_number
+        return count_room - 1
     else:
-        data = list(chain.from_iterable(data))
-        data1 = len(data)
-        data = set(data)
-        data2 = len(data)
-        room_number = data1 - data2 + 1
-        return room_number
+        for time in range(len(data) - 1):
+            if data[time][1] >= data[time + 1][0]:
+                count_room += 1
+        return count_room
