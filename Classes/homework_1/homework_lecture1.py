4d94781e31f97003c24e3f8775501e427aed7547
Valentyn-Lukashuk <Valentynlukashuk@gmail.com>
2020-10-14 18:51:39 +0300

    add_classes

--- Classes/homework_1/homework_lecture1.py
+++ Classes/homework_1/homework_lecture1.py
@@ -1,41 +1,28 @@
 class CustomInt(int):
-
-    def __init__(self, x):
-        self.x = x
-
-
-    def __lt__(self, other):
-        return self.x > self.x
-
-    def __gt__(self, other):
-        return self.x < other.x
-
-    def __le__(self, other):
-        return self.x >= other.x
-
-    def __ge__(self, other):
-        return self.x <= other.x
+    """
+    Make class for custom int, which will behave "vice versa" for comparison operations:
+    e.g., int < int means less then, but custom int < int should equal custom int > int.
+    Take into account <, >, >=, <=
+    ** try to achieve this without __init__ rewriting
+    """
 
 
 class PersonWithLimitedSkills:
-    __slots__ = ("eat", "sleep")
-
-
-
-
+    """
+    Make class which is limited to 2 actions - eat and sleep
+    Any other attributes addition should result in an error.
+    """
 
 
 class HiddenAttrs:
-    def __dir__(self):
-        return []
-
-    def __dict__(self):
-        return {}
+    """
+    Make class which never tells about its attributes.
+    Its attribute list is always empty and attributes dictionary is always empty.
+    """
 
 
 class CallableInstances:
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self, *args, **kwargs):
-        return self.func(*args, **kwargs)
+    """
+    Make class which takes func parameter on initialization, which is a callable that can be passed.
+    Then object of this class may be called - callable passed on init will be called with passed parameters.
+    """
