7ca98af10543fd2250eaec758792d79104f95f85
oksanamelashenko <oksanich777@icloud.com>
2020-10-21 23:16:24 +0300

    HW#4 homework_lecture1.py

--- Classes/homework_1/homework_lecture1.py
+++ Classes/homework_1/homework_lecture1.py
@@ -6,12 +6,28 @@ class CustomInt(int):
     ** try to achieve this without __init__ rewriting
     """
 
+    def __init__(self, custom_int: Any) -> None
+        self.custom_int = custom_int
+
+    def __ge__(self, other):
+        return self.custom_int <= other.custom_int
+
+    def __gt__(self, other):
+        return self.custom_int < other.custom_int
+
+    def __le__(self, other):
+        return self.custom_int >= other.custom_int
+
+    def __lt__(self, other):
+        return self.custom_int > self.custom_int
+
 
 class PersonWithLimitedSkills:
     """
     Make class which is limited to 2 actions - eat and sleep
     Any other attributes addition should result in an error.
     """
+    __slots__ = ('eat', 'sleep')
 
 
 class HiddenAttrs:
@@ -20,9 +36,21 @@ class HiddenAttrs:
     Its attribute list is always empty and attributes dictionary is always empty.
     """
 
+    def __dir__(self):
+        return []
+
+    def __dict__(self):
+        return {}
+
 
 class CallableInstances:
     """
     Make class which takes func parameter on initialization, which is a callable that can be passed.
     Then object of this class may be called - callable passed on init will be called with passed parameters.
     """
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, x):
+        return func(x)
