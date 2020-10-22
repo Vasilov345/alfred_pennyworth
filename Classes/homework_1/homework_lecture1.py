2e6a39125aae53fc292fda024ba0e43edfe1caa8
VolodymyrKM <kolomiecnew@gmail.com>
2020-10-22 19:11:56 +0300

    home_work_4 done

--- Classes/homework_1/homework_lecture1.py
+++ Classes/homework_1/homework_lecture1.py
@@ -5,13 +5,46 @@ class CustomInt(int):
     Take into account <, >, >=, <=
     ** try to achieve this without __init__ rewriting
     """
+    def __init__(self, a):
+        self.a = a
+
+
+    def __lt__(self, other):
+        if isinstance(other, (int, float)):
+            if self.a < other:
+                return False
+
+
+    def __le__(self, other):
+        if isinstance(other, (int, float)):
+            if self.a <= other:
+                return False
+
+    def __ge__(self, other):
+        if isinstance(other, (int, float)):
+            if self.a >= other:
+                return False
+
+    def __gt__(self, other):
+        if isinstance(other, (int, float)):
+            if self.a > other:
+                return False
+
 
 
 class PersonWithLimitedSkills:
     """
-    Make class which is limited to 2 actions - eat and sleep
-    Any other attributes addition should result in an error.
-    """
+       Make class which is limited to 2 actions - eat and sleep
+       Any other attributes addition should result in an error.
+       """
+    __slots__ = ()
+
+    def eat(self):
+        print('eat')
+
+    def sleep(self):
+        print('sleep')
+
 
 
 class HiddenAttrs:
@@ -19,6 +52,12 @@ class HiddenAttrs:
     Make class which never tells about its attributes.
     Its attribute list is always empty and attributes dictionary is always empty.
     """
+    def __dir__(self):
+        return []
+
+    def __dict__(self):
+        return {}
+
 
 
 class CallableInstances:
@@ -26,3 +65,8 @@ class CallableInstances:
     Make class which takes func parameter on initialization, which is a callable that can be passed.
     Then object of this class may be called - callable passed on init will be called with passed parameters.
     """
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
