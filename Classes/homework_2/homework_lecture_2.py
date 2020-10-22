884177bd8cc4831a28216fd676e8ccbf9657f562
VolodymyrKM <kolomiecnew@gmail.com>
2020-10-22 19:49:11 +0300

    homework_5 done

--- Classes/homework_2/homework_lecture_2.py
+++ Classes/homework_2/homework_lecture_2.py
@@ -4,6 +4,10 @@ class Maths:
     Note: this method should not take instance as first parameter.
     """
 
+    @staticmethod
+    def add_num(x, y):
+        return x + y
+
 
 class Pizza:
     """
@@ -18,28 +22,70 @@ class Pizza:
         pizza2.ingredients will equal to ['mozzarella', 'tomatoes', 'ham']
     """
 
+    def __init__(self, ingridients):
+        self.ingredients = ingridients
 
+    @classmethod
+    def margherita(cls):
+        return cls([ 'mozzarella', 'tomatoes' ])
+
+    @classmethod
+    def prosciutto(cls):
+        return cls([ 'mozzarella', 'tomatoes', 'ham' ])
+
+
+# """
+# Make class, which has max_visitors_num attribute and its instances will have visitors_count attribute.
+# In case of setting visitors_count - max_visitors_num should be checked,
+# if visitors_count value is bigger than max_visitors_num - visitors_count should be assigned with max_visitors_num.
+# Example:
+#     Concert.max_visitor_num = 50
+#     concert = Concert()
+#     concert.visitors_count = 1000
+#     print(concert.visitors_count)  # 50
+# """
 class Concert:
-    """
-    Make class, which has max_visitors_num attribute and its instances will have visitors_count attribute.
-    In case of setting visitors_count - max_visitors_num should be checked,
-    if visitors_count value is bigger than max_visitors_num - visitors_count should be assigned with max_visitors_num.
-    Example:
-        Concert.max_visitor_num = 50
-        concert = Concert()
-        concert.visitors_count = 1000
-        print(concert.visitors_count)  # 50
-    """
+    max_visitors_num = 0
+
+    def __init__(self):
+        self._visitors_count = 0
+
+    @property
+    def visitors_count(self):
+        return self._visitors_count
+
+    @visitors_count.setter
+    def visitors_count(self, value):
+        if value > Concert.max_visitors_num:
+            self._visitors_count = Concert.max_visitors_num
+        else:
+            self._visitors_count = value
 
 
+from dataclasses import dataclass
+
+
+@dataclass
 class BookDataclass:
     """
-    Create dataclass with 3 fields - title (str), author (str), pages_num (int)
-    """
+            Create dataclass with 3 fields - title (str), author (str), pages_num (int)
+            """
+
+    title: str
+    author: str
+    pages_num: int = 0
 
 
 class Book:
     """
-    Create regular class taking 3 params on init - title, author, pages_num
-    Make its str() representation the same as for BookDataclass defined above.
-    """
+           Create regular class taking 3 params on init - title, author, pages_num
+           Make its str() representation the same as for BookDataclass defined above.
+           """
+
+    def __init__(self, title, author, pages_num):
+        self.title = title
+        self.author = author
+        self.pages_num = pages_num
+
+    def __str__(self):
+        return str(BookDataclass(title=self.title, author=self.author, pages_num=self.pages_num))
