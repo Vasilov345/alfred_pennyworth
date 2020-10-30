a55b96f1a753748f276b0af81ed1196a345604b2
Chepak_Semen <chepak.semmy@gmail.com>
2020-10-30 15:19:57 +0200

    Done Jungle_v:1

--- Classes/homework_3/jungle.py
+++ Classes/homework_3/jungle.py
@@ -1,27 +1,29 @@
-from random import randint
-from typing import Any
+from __future__ import annotations
+from uuid import uuid4
 from abc import ABC, abstractmethod
+from random import randint
+import time
 
 
 class Animal(ABC):
-    def __init__(self):
-        self.id = id
-        power = randint(20, 100)
-        self.speed = randint(20, 100)
-        self.current_power = power
+
+    def __init__(self, power: int, speed: int):
+        self.id = uuid4()
         self.max_power = power
+        self.current_power = power
+        self.speed = speed
 
     @abstractmethod
-    def eat(self, jungle):
+    def eat(self, jungle: Jungle):
         raise NotImplementedError
 
 
 class Predator(Animal):
 
-    def eat(self, jungle):
+    def eat(self, jungle: Jungle):
         count_animals = len(jungle.animals.keys())
         force_key = randint(1, count_animals)
-        force_animal: Any[Predator, Herbivorous] = jungle.animals[force_key]
+        force_animal = jungle.animals[force_key]
         if force_animal.id == self.id:
             self.current_power -= (self.max_power / 100) * 40
         else:
@@ -37,7 +39,7 @@ class Predator(Animal):
 
 class Herbivorous(Animal):
 
-    def eat(self, jungle):
+    def eat(self, jungle: Jungle):
         if self.current_power <= 0:
             jungle.remove_animal(self)
         self.current_power += (self.max_power / 100) * 40
@@ -46,42 +48,46 @@ class Herbivorous(Animal):
         return self.current_power
 
 
+
+
 class Jungle:
+
     def __init__(self):
-        self.animals = dict()  # Створили словник з
+        self.animals = dict()
 
-    def add_animal(self, animals_from_gen):
-        # створюємо для кожної тварини число як ключ
-        key = len(self.animals.keys()) + 1
-        self.animals[key] = animals_from_gen
+    def add_animal(self, animal):
+        self.animals[animal.id] = animal
+        return self.animals
 
-    def remove_animal(self, jungle):
-        animal_list_values = [a for a in self.animals.values()]
-        animal_list_keys = [k for k in self.animals.keys()]
-        index_of_animal = animal_list_values.index(animal)
-        key = animal_list_keys[index_of_animal]
-        del self.animals[key]
+    def remove_animal(self, animal):
+        self.animals.pop(animal.id)
+        return self.animals
+
+    def any_predator_left(self):
+        count = 0
+        if any(isinstance(animal, Predator) for self.animals in jungle.animals.values()):
+            return True
+        else:
+            False
 
 
 def animal_generator():
-    animals_from_gen = [Predator, Herbivorous]
-    for i in range(1, 5):
-        yield animals_from_gen[randint(0, 1)]()
+    animals =[Predator, Herbivorous]
+    for i in range(10):
+        yield animals[randint(0, 1)](randint(20, 100), randint(20, 100))
 
 
-jungle = Jungle()
-for animal in animal_generator():
-    jungle.add_animal(animal)
 
-    while jungle.animals:
-        is_predator = bool([animal for animal in jungle.animals.values() if animal.__class__.__name__ == 'Predator'])
+if __name__ == "__main__":
+    nature = animal_generator()
+    jungle = Jungle()
+    for i in range(10):
+        animal = next(nature)
+        jungle.add_animal(animal)
 
-        if is_predator == False:
-            print('The end')
-            print(animal, animal.current_power)
+    while True:
+        if not jungle.any_predator_left():
             break
-
-        count_animals = len(jungle.animals.keys())
-        animal = jungle.animals[randint(1, count_animals)]
-
-        animal.eat(jungle)
+        for animal in jungle.animals:
+            animal.eat(jungle=jungle)
+        time.sleep(1)
--- LoggingAndExceptions/homework/homework.py
+++ LoggingAndExceptions/homework/homework.py
@@ -1,9 +1,12 @@
+import logging
+
+
 class Error(Exception):
     pass
 
 
 class TooManyVisitors(Error):
-    pass
+   pass
 
 
 class TooFewVisitors(Error):
@@ -13,12 +16,18 @@ class TooFewVisitors(Error):
 class Concert:
 
     # add 2 class attributes - max_visitors (200) and min_visitors (10)
-
+    max_visitors = 200
+    min_visitors = 10
     def __init__(self, visitors_num):
         """
         if visitors num is bigger than max_visitors - raise TooManyVisitors error
         if visitors num is less than min_visitors - raise TooFewVisitors error
         """
+        self.visitors_num = visitors_num
+        if self.visitors_num > self.max_visitors:
+            raise TooManyVisitors
+        elif self.visitors_num < self.min_visitors:
+            raise TooFewVisitors
 
 
 def make_concert(visitors_num):
@@ -26,6 +35,15 @@ def make_concert(visitors_num):
     create Concert instance - handle TooManyVisitors and TooFewVisitors errors here:
     in case if caught - log error to console and return False, in case of successful initialization - return True
     """
+    try:
+        a = Concert(visitors_num)
+    except TooFewVisitors:
+        return False
+    except TooManyVisitors:
+        return False
+    else:
+        return True
+
 
 # create Logger object
 # set level to debug
@@ -39,3 +57,11 @@ def log_message(message, level):
     :param message:
     :param level:
     """
+    logging.basicConfig(filename="test.log", filemode="w")
+    if level > 30:
+        logging.info(message)
+        else
+        logging.info([])
+
+if __name__ == "__main__":
+    a =Concert(30)
