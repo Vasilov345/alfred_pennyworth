de597b455f9f05510ab998b23260b533a4ae43fd
Semen <72144294+Chepak-Semen@users.noreply.github.com>
2020-11-01 20:42:49 +0200

    Add files via upload

--- Classes/homework_3/jungle.py
+++ Classes/homework_3/jungle.py
@@ -1,87 +1,142 @@
-from random import randint
-from typing import Any
+from __future__ import annotations
+from uuid import uuid4
 from abc import ABC, abstractmethod
+from random import randint, choice
+import csv
 
 
 class Animal(ABC):
-    def __init__(self):
-        self.id = id
-        power = randint(20, 100)
-        self.speed = randint(20, 100)
-        self.current_power = power
+
+    def __init__(self, power: int, speed: int):
+        self.id = str(uuid4())
         self.max_power = power
+        self.current_power = power
+        self.speed = speed
 
     @abstractmethod
-    def eat(self, jungle):
+    def eat(self, jungle: Jungle):
         raise NotImplementedError
+
+    def animal_can_search_food(self):
+        return self.current_power > 0
+
+    def loss_power(self):
+        self.current_power = self.current_power - int(self.max_power*0.3)
+
+    def restore_power(self):
+        self.current_power = min(self.max_power, self.current_power + int(self.max_power*0.4))
+        if self.current_power > self.max_power:
+            self.current_power = self.max_power
+
 
 
 class Predator(Animal):
 
+
     def eat(self, jungle):
-        count_animals = len(jungle.animals.keys())
-        force_key = randint(1, count_animals)
-        force_animal: Any[Predator, Herbivorous] = jungle.animals[force_key]
-        if force_animal.id == self.id:
-            self.current_power -= (self.max_power / 100) * 40
-        else:
-            if self.speed > force_animal.speed and self.current_power > force_animal.current_power:
-                self.current_power += (self.max_power / 100) * 40
-                jungle.animals.pop(force_key)
-                if self.current_power > self.max_power:
-                    self.current_power = self.max_power
+
+        if not self.animal_can_search_food():
+            jungle.remove_animal(self)
+            return
+
+        prey_animal = jungle.get_rand_animal()
+
+        if prey_animal.id == self.id:
+            prey_animal.loss_power()
+
+        if self.catch(prey_animal):
+
+            if self.kill(prey_animal):
+                self.restore_power()
+                jungle.remove_animal(prey_animal)
             else:
-                self.current_power -= (self.max_power / 100) * 40
-                return self.current_power
+                self.loss_power()
+                prey_animal.loss_power()
+                if not self.animal_can_search_food():
+                    jungle.remove_animal(self)
+                    return
+
+        else:
+            self.loss_power()
+            prey_animal.loss_power()
+            if not self.animal_can_search_food():
+                jungle.remove_animal(self)
+                return
+
+
+    def catch(self, prey_animal):
+        return prey_animal.speed < self.speed
+
+    def kill(self, prey_animal):
+        return prey_animal.current_power < self.current_power
 
 
 class Herbivorous(Animal):
 
-    def eat(self, jungle):
-        if self.current_power <= 0:
+    def eat(self, jungle: Jungle):
+        if not self.animal_can_search_food():
             jungle.remove_animal(self)
-        self.current_power += (self.max_power / 100) * 40
-        if self.current_power > self.max_power:
-            self.current_power = self.max_power
-        return self.current_power
+            return
+        else:
+            self.restore_power()
 
 
 class Jungle:
+
     def __init__(self):
-        self.animals = dict()  # Створили словник з
+        self.animals = dict()
+        self.number = -1
 
-    def add_animal(self, animals_from_gen):
-        # створюємо для кожної тварини число як ключ
-        key = len(self.animals.keys()) + 1
-        self.animals[key] = animals_from_gen
+    def __getitem__(self, item):
+        leng = len(self.animals)
+        if self.number >= leng -1:
+            self.number = -1
+            raise StopIteration
+        self.number += 1
+        return list(self.animals.values())[self.number]
 
-    def remove_animal(self, jungle):
-        animal_list_values = [a for a in self.animals.values()]
-        animal_list_keys = [k for k in self.animals.keys()]
-        index_of_animal = animal_list_values.index(animal)
-        key = animal_list_keys[index_of_animal]
-        del self.animals[key]
+    def any_predator_left(self):
+        if any(isinstance(animal, Predator) for animal in jungle.animals.values()):
+            return True
+
+    def add_animal(self, animal: Animal):
+        self.animals[animal.id] = animal
+
+    def remove_animal(self, animal: Animal):
+        self.animals.pop(animal.id)
+
+    def get_rand_animal(self):
+        return choice(list(jungle.animals.values()))
 
 
 def animal_generator():
-    animals_from_gen = [Predator, Herbivorous]
-    for i in range(1, 5):
-        yield animals_from_gen[randint(0, 1)]()
+    animals = [Predator, Herbivorous]
+    for i_animals in range(10):
+        yield animals[randint(0, 1)](power=randint(20, 100), speed=randint(20, 100))
+
+def creator_CSV(jungle):
+    lst =[]
+    for k, v in jungle.animals.items():
+        list_animals = [f"{v.__class__.__name__}", f"{v.max_power}", f"{v.speed}", f"{v.id}"]
+        lst.append(list_animals)
+    with open("animals.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(['class', 'power', 'speed', 'id'])
+        writer.writerows(lst)
 
 
-jungle = Jungle()
-for animal in animal_generator():
-    jungle.add_animal(animal)
+if __name__ == "__main__":
+    nature = animal_generator()
 
-    while jungle.animals:
-        is_predator = bool([animal for animal in jungle.animals.values() if animal.__class__.__name__ == 'Predator'])
-
-        if is_predator == False:
-            print('The end')
-            print(animal, animal.current_power)
+    jungle = Jungle()
+    for i in range(10):
+        animal = next(nature)
+        jungle.add_animal(animal)
+    creator_CSV(jungle)
+    while True:
+        if not jungle.any_predator_left():
+            print(f"list of Herbivorous left {jungle.animals}")
             break
+        for animal in jungle:
+            animal.eat(jungle=jungle)
 
-        count_animals = len(jungle.animals.keys())
-        animal = jungle.animals[randint(1, count_animals)]
-
-        animal.eat(jungle)
