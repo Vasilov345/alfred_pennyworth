954df9ba5852a83677b0385320c9e53af35ce427
VolodymyrKM <kolomiecnew@gmail.com>
2020-11-01 13:19:39 +0200

    home_work_#6_#7 done

--- Classes/homework_3/jungle.py
+++ Classes/homework_3/jungle.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
-from abc import ABC, abstractmethod
-from uuid import uuid4
-from random import randint, choice
 from typing import Dict, Any
+from abc import ABC, abstractmethod
+from random import randint, choice
+from uuid import uuid4
+import csv
 
 
 class Animal(ABC):
@@ -20,75 +21,75 @@ class Animal(ABC):
 
 class Predator(Animal):
 
-    def eat(self, other):
-        other = choice(list(other.values()))
-        print(self.current_power)
-        print(other.current_power)
-        if self.id == other.id:
+    def eat(self, jungle: Jungle):
+        if self.current_power <= 0:
+            jungle.remove_animal(self)
+        choice_from_jun = choice(list(jungle.animals.values()))
+        if self.id == choice_from_jun.id:
             self.current_power = self.current_power
-        elif self.speed < other.speed or self.current_power < other.current_power:
-            self.current_power -= (self.max_power * 30) / 100
-            other.current_power -= (other.max_power * 30) / 100
-            if self.current_power <= 0:
-                self.current_power = 0
-                print(self.id)
-                obj_Jung.remove_animal(self)
-                print(self.id)
-            if other.current_power <= 0:
-                other.current_power = 0
-                obj_Jung.remove_animal(other)
+        elif self.speed < choice_from_jun.speed or self.current_power < choice_from_jun.current_power:
+            self.current_power -= int(self.max_power * 0.3)
+            choice_from_jun.current_power -= int(choice_from_jun.max_power * 0.3)
         else:
-            self.current_power += (self.max_power * 40) / 100
-            if self.current_power >= self.max_power:
-                self.current_power = self.max_power
-
-        print(self.current_power)
-        print(other.current_power)
-
-
-class Herbivorous(Animal):
-
-    def eat(self):
-        self.current_power += (self.max_power * 40) / 100
+            self.current_power += int(self.max_power * 0.4)
+            jungle.remove_animal(choice_from_jun)
         if self.current_power >= self.max_power:
             self.current_power = self.max_power
 
 
-#
-# AnyAnimal = Any[ Herbivorous, Predator ]
+class Herbivorous(Animal):
+
+    def eat(self, jungle: Jungle):
+        if self.current_power <= 0:
+            jungle.remove_animal(self)
+        else:
+            self.current_power += int(self.current_power + self.max_power * 0.4)
+        if self.current_power > self.max_power:
+            self.current_power = self.max_power
 
 
 class Jungle:
 
     def __init__(self):
-        self.animals: Dict[ str, AnyAnimal ] = dict()
-        self.animals_2 = self.animals
+        self.animals: Dict[ str, Animal ] = dict()
+        self.number = -1
 
-    def add_animal(self, animal: AnyAnimal):
+    def __getitem__(self, item):
+        length = len(self.animals)
+        if self.number >= length - 1:
+            self.number = -1
+            raise StopIteration
+        self.number += 1
+        return list(self.animals.values())[ self.number ]
+
+    def add_animal(self, animal: Animal):
         self.animals[ animal.id ] = animal
 
-    def remove_animal(self, animal: AnyAnimal):
+    def remove_animal(self, animal: Animal):
         self.animals.pop(animal.id)
 
-
-obj_Jung = Jungle()
+    def any_predator_left(self):
+        if any(isinstance(obj, Predator) for obj in self.animals.values()):
+            return True
+        return False
 
 
 def animal_generator():
     for i in range(10):
         if i % 2 == 0:
-            i = Predator(randint(20, 101), randint(20, 101))
+            i = Predator(power=randint(20, 100), speed=randint(20, 100))
             i.id = uuid4()
             yield i
-        elif i % 2 == 1:
-            i = Herbivorous(randint(20, 101), randint(20, 101))
+        else:
+            i = Herbivorous(power=randint(20, 100), speed=randint(20, 100))
             i.id = uuid4()
             yield i
 
+def dict_animal_writer(dictionary: dict):
+    with open('animal.csv', 'w') as f:
+        for key, value in dictionary.items():
+            f.write('id: ' + str(key) + '\t' + str(value.__class__) + ' speed: ' + str(value.speed) + ' power: ' + str(value.current_power) +'\n')
 
-for i in animal_generator():
-    obj_Jung.add_animal(i)
-print(obj_Jung.animals)
 
 
 if __name__ == "__main__":
@@ -97,4 +98,21 @@ if __name__ == "__main__":
     # Add animals to jungle
     # Iterate throw jungle and force animals to eat until no predators left
     # animal_generator to create a random animal
-    pass
+
+    nature = animal_generator()
+    jungle = Jungle()
+
+    for i in range(10):
+        animal = next(nature)
+        jungle.add_animal(animal)
+    print(jungle.animals)
+
+    dict_animal_writer(jungle.animals)
+
+    while True:
+        if not jungle.any_predator_left():
+            break
+        for animal in jungle:
+            animal.eat(jungle=jungle)
+
+    # print(jungle.animals)
