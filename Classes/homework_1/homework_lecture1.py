9a2aa4b0b1e83ebf3e07b0c76752aea806421210
ruslan <rusign29@gmail.com>
2020-11-04 21:51:50 +0200

    homework_lecture1.py

--- Classes/homework_1/homework_lecture1.py
+++ Classes/homework_1/homework_lecture1.py
@@ -1,3 +1,6 @@
+from typing import Tuple, Callable
+
+
 class CustomInt(int):
     """
     Make class for custom int, which will behave "vice versa" for comparison operations:
@@ -6,12 +9,28 @@ class CustomInt(int):
     ** try to achieve this without __init__ rewriting
     """
 
+    def __ge__(self, other):
+        print(other)
+    def __le__(self, other):
+        print(other)
+    def __lt__(self, other):
+        print(other)
+    def __gt__(self, other):
+        print(other)
+
 
 class PersonWithLimitedSkills:
     """
     Make class which is limited to 2 actions - eat and sleep
     Any other attributes addition should result in an error.
     """
+    def eat(self,eat):
+        return eat
+    def sleep(self,sleep):
+        return sleep
+
+        __slots__=(eat,sleep)
+
 
 
 class HiddenAttrs:
@@ -19,6 +38,10 @@ class HiddenAttrs:
     Make class which never tells about its attributes.
     Its attribute list is always empty and attributes dictionary is always empty.
     """
+    def __dir__(self):
+        return []
+    def __dict__(self):
+        return {}
 
 
 class CallableInstances:
@@ -26,3 +49,7 @@ class CallableInstances:
     Make class which takes func parameter on initialization, which is a callable that can be passed.
     Then object of this class may be called - callable passed on init will be called with passed parameters.
     """
+    def __init__(self, function):
+        self.function= function
+    def __call__(self, *args, **kwargs):
+        return self.function(*args,**kwargs)
