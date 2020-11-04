e7e91bf49a7dd2886a5600da9e55d3829de14773
bezuhlyiv <karamboll509@gmail.com>
2020-11-04 21:45:41 +0200

    HW#6 Done

--- Classes/homework_3/jungle_solved.py
+++ Classes/homework_3/jungle_solved.py
@@ -1,76 +1,57 @@
 from __future__ import annotations
-import abc
 import uuid
 import random
+from abc import ABC, abstractmethod
 
-from typing import Dict
 
+class Animal(ABC):
 
-class Animal(abc.ABC):
-
-    def __init__(self, power: int, speed: int):
+    def __init__(self, power, speed):
         self.id = str(uuid.uuid4())
         self.max_power = power
         self.current_power = power
         self.speed = speed
 
-    def restore_power(self):
-        self.current_power = min(self.max_power, self.current_power + int(self.max_power * 0.4))
-
-    def loss_power(self):
-        self.current_power = self.current_power - int(self.max_power * 0.3)
-
-    @abc.abstractmethod
+    @abstractmethod
     def eat(self, jungle: Jungle):
-        """Method eat for animal
-        """
-
-    def if_animal_can_search_food(self):
-        return self.current_power > 0
-
-    def catch(self, prey: Animal):
-        return prey.speed < self.speed
-
-    def kill(self, prey: Animal):
-        return prey.current_power < self.current_power
+        pass
 
 
 class Predator(Animal):
-    def eat(self, jungle: Jungle):
-        if not self.if_animal_can_search_food():
-            jungle.remove_animal(self)
-            return
 
-        prey = jungle.get_random_animal()
-        if prey.id == self.id:
-            self.loss_power()
-            return
-        is_killed = False
-        is_caught = False
-        if self.catch(prey):
-            is_caught = True
-            if self.kill(prey):
-                jungle.remove_animal(prey)
-                is_killed = True
-        if not is_caught or not is_killed:
-            self.loss_power()
-            prey.loss_power()
+    def eat(self, jungle: Jungle):
+        any_animal = random.choice([x for x in jungle.animals.values()])
+        if self.current_power <= 0:
+            jungle.remove_animal(self)
+        elif any_animal.id == self.id:
+            self.current_power -= 0.3 * self.max_power
+        else:
+            if self.speed > any_animal.speed and self.current_power > any_animal.current_power:
+                self.current_power += 40
+                if self.current_power > self.max_power:
+                    self.current_power = self.max_power
+            else:
+                self.current_power -= 0.3 * self.max_power
+                any_animal.current_power -= 0.3 * any_animal.max_power
 
 
 class Herbivorous(Animal):
 
     def eat(self, jungle: Jungle):
-        if self.if_animal_can_search_food():
-            self.restore_power()
-        else:
+        if self.current_power <= 0:
             jungle.remove_animal(self)
+        else:
+            self.current_power += 40
+            if self.current_power > self.max_power:
+                self.current_power = self.max_power
 
 
 class Jungle:
 
     def __init__(self):
-        self.animals: Dict[str, Animal] = dict()
-        self.number = -1
+        self.animals = dict()
+
+    number: int = - 1
 
     def __getitem__(self, item):
         length = len(self.animals)
@@ -80,37 +61,39 @@ class Jungle:
         self.number += 1
         return list(self.animals.values())[self.number]
 
-    def get_random_animal(self):
-        return random.choice(list(self.animals.values()))
-
-    def any_predator_left(self):
-        if any(isinstance(animal, Predator) for animal in jungle.animals.values()):
-            return True
-        return False
-
-    def add_animal(self, animal: Animal):
+    def add_animal(self, animal):
         self.animals[animal.id] = animal
 
-    def remove_animal(self, animal: Animal):
+    def remove_animal(self, animal):
         self.animals.pop(animal.id)
+        return self.animals
 
 
 def animal_generator():
-    while True:
+    for random_animal in range(10):
         if random.randint(1, 2) == 1:
-            yield Herbivorous(power=random.randint(20, 100), speed=random.randint(20, 100))
-        else:
             yield Predator(power=random.randint(20, 100), speed=random.randint(20, 100))
+        else:
+            yield Herbivorous(power=random.randint(20, 100), speed=random.randint(20, 100))
 
 
 if __name__ == "__main__":
-    gen = animal_generator()
-    jungle = Jungle()
-    for i in range(5):
-        jungle.add_animal(next(gen))
-
-    while True:
-        if not jungle.any_predator_left():
-            break
-        for animal in jungle:
-            animal.eat(jungle=jungle)
+    # jungle = Jungle()
+    # nature = animal_generator()
+    #
+    # while True:
+    #     try:
+    #         jungle.add_animal(next(nature))
+    #     except StopIteration:
+    #         break
+    #
+    # while True:
+    #     if any(isinstance(animal, Predator) for animal in jungle.animals.values()):
+    #         try:
+    #             for animal in jungle.animals.values():
+    #                 animal.eat(jungle=jungle)
+    #         except RuntimeError:
+    #             continue
+    #     else:
+    #         break
+    pass
