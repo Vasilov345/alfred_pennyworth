2344f13e6ed344c8b13c2770be1a9608f18bb0d4
Oleksandr Yurchyk <yurchyk_sasha@ukr.net>
2020-11-21 11:59:15 +0200

    snake after lesson

--- ThreadedSnake/snake.py
+++ ThreadedSnake/snake.py
@@ -11,14 +11,35 @@ class Snake(list):
     def __init__(self):
         super().__init__()
         self.extend([Point(5, 5), Point(5, 6), Point(5, 7)])
+        self.direction = 'R'
+
+    @property
+    def head(self):
+        return self[-1]
+
+    def move(self):
+        if self.direction == 'R':
+            self.pop(0)
+            self.append((Point(self.head.y, self.head.x + 1)))
+
+
+def print_snake(screen, snake):
+    for point in snake:
+        screen.addch(*point, '#')
+
+
+def updating(screen, snake, num):
+    for n in range(num):
+        snake.move()
+        print_snake(screen, snake)
+        screen.refresh()
+        sleep(1)
+        screen.clear()
 
 
 def main(screen):
-    for i in Snake():
-        screen.addch(*i, '#')
-        sleep(1)
-    screen.refresh()
-    # screen.clear()
+    snake = Snake()
+    updating(screen, snake, 10)
     sleep(5)
 
 
